8be47ab87551942fd9150ef8e8600f6b271207db
Akramjon <akramjon0175@gmail.com>
2023-07-29 10:44:44 -0700

    Initial Commit

---
+++ Clone/CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneApp.swift
+//  Clone
+//
+//  Created by Akramjon on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
